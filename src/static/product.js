5832b73d4e02c0867baecdfd09c676ab118940e6
Afif <afifwoles04@gmail.com>
2022-06-26 01:33:54 +0700

    update

---
+++ src/static/product.js
@@ -0,0 +1,10 @@
+export default {
+    Products: [
+        { nama: 'Buku', stok: 5000 },
+        { nama: 'Bolpoin', stok: 0 },
+        { nama: 'Pensil', stok: 3000 },
+        { nama: 'Penggaris', stok: 7000 },
+        { nama: 'Penghapus', stok: 0 },
+        { nama: 'Papan Tulis', stok: 60000 },
+    ],
+}
---
+++ src/view/TablePage.vue
@@ -0,0 +1,47 @@
+<template>
+<table class="table table-striped table-bordered">
+      <thead>
+        <tr>
+          <th>Nama</th>
+          <th>Stok</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr  v-for="item in items" :key="item">
+          <!-- <th scope="row">1</th> -->
+          <td>{{ item.nama }}</td>
+          <td>{{ item.stok }}</td>
+          <td v-if="item.stok == 0">Tidak Tersedia</td>
+          <td v-else>Tersedia</td>
+        </tr>
+        <!-- <td>
+          <tr v-for="item in items" :key="item">{{ item.nama }}</tr>
+        </td>
+        <td>
+          <tr v-for="item in items" :key="item">{{ item.stok }}</tr>
+        </td>
+        <td>
+          <tr v-for="item in items" :key="item">{{ item.stok >= 0 ? "Tidak Tersedia" : "Tersedia" }}</tr>
+        </td> -->
+      </tbody>
+
+
+    </table>
+</template>
+
+<script>
+import file from "@/static/product.js"
+export default {
+  data() {
+    return {
+      name: 'App',
+      items: file.Products,
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
